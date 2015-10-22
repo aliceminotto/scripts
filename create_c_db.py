e853e203ef0ce47e20c58be9b98802d004c67d21
aliceminotto <i.m.e@hotmail.it>
2015-10-22 11:20:20 +0100

    add index to make lines unique in first table

--- create_c_db.py
+++ create_c_db.py
@@ -15,22 +15,31 @@ conn.execute('''create table mutation_probabilities
 	m6 real not null,
 	m7 real not null,
 	m8 real not null);''')
+conn.execute('''create unique index prob
+	on mutation_probabilities (hgt_Eff,hgt_Tes,Eff_mutation,Tes_duplication,m5,m6,m7,m8)
+	''')
 conn.execute('''create table inputs
 	(id integer primary key autoincrement,
 	NJumps int not null,
 	DT int not null,
 	Seed int not null);''')
+#print '**' #checkpoint
 conn.execute('''create table output
-	(id_probabilities int not null,
-	id_input int not null,
+	(id_probabilities int,
+	id_input int,
 	Run int not null,
 	C int not null,
 	N int not null,
-	output array);''')
+	output array,
+	foreign key(id_probabilities) references mutation_probabilities(id),
+	foreign key(id_input) references inputs(id));''')
+#print '*'  #checkpoint
 conn.execute('''create table plots
-	(id_probabilities int not null,
-	id_input int not null,
+	(id_probabilities int,
+	id_input int,
 	figure blob,
-	constraint pk_id primary key (id_probabilities,id_input));''')
+	constraint pk_id primary key (id_probabilities,id_input),
+	foreign key(id_probabilities) references mutation_probabilities(id),
+	foreign key(id_input) references inputs(id));''')
 
 conn.close()
