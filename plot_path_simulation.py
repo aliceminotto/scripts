8c670b690597ed73c9b7c3e32d4a10ef9215f0bd
aliceminotto <i.m.e@hotmail.it>
2015-10-30 13:33:50 +0000

    changed style

--- plot_path_simulation.py
+++ plot_path_simulation.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 from pylab import *
 import argparse
 parser=argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,epilog=("""
@@ -17,27 +18,33 @@ A=pickle.load(open(data,"rb"))
 close(data)
 print("LOADED")
 
-fig = plt.figure()
 To=0
 Tmx=len(A[0][1]) #time considered
 Tf=Tmx-1
 y1=0.0
 y2=10000.0
 tprt=10
-plt.style.use('fivethirtyeight')
+plt.style.use('bmh')
+fig, axesa = plt.subplots(1,figsize=(10, 8))
+
+axesa.set_ylabel("$Populations$", fontsize=40)
+axesa.set_xlabel("$Time$ $(Evolutionary$ $events)$",fontsize=40)
+axesa.xaxis.set_tick_params(labelsize=20)
+axesa.xaxis.set_major_formatter(mtick.FormatStrFormatter('%2.2e'))
+axesa.yaxis.set_tick_params(labelsize=20)
+axesa.yaxis.set_major_formatter(mtick.FormatStrFormatter('%2.2e'))
+
 
 for strain in A.keys():
     x=A[strain][1] #time
     y=A[strain][0] #pop size'''
-    plt.plot(x,y, linewidth=0.5)#'-o')
-#print x
-#print y
-#raw_input()
+    plt.plot(x,y,'-', linewidth=0.9)#'-o')
+
 '''y=A[0][0]
 x=A[0][1]'''
 
 #plt.xlim([0,200])
-plt.ylim([0,100])
+#plt.ylim([0,1000])
 #plt.xlabel("time  (generations)",fontsize=25)
 #plt.ylabel("Pop sizes",fontsize=25)
 #formatter = ScalarFormatter()
