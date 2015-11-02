6d1fa10881accf4471e23dd6f7e0b1e1b8121200
aliceminotto <i.m.e@hotmail.it>
2015-11-02 14:54:40 +0000

    correct solution, could be shorter

--- mortal_fibonacci_rabbits.py
+++ mortal_fibonacci_rabbits.py
@@ -1,23 +1,28 @@
 #!usr/bin/python
 import sys
-'''f=sys.argv[1]
-F=open(f,'r')'''
+from collections import deque
 
 def fibonacci_variant(T):
     if T==0 or T<0:
         N=0
-    elif T==1 or T==2:
+    elif T==1:
         N=1
+        age_pop.pop()
+        age_pop.appendleft(1)
     else:
-        N=fibonacci_variant(T-1)+fibonacci_variant(T-2)-(fibonacci_variant(T-m)-fibonacci_variant(T-2*m))
-    return N
-'''for line in F.readlines():
-    par=line.split()
-n=par[0] #number of month
-m=par[1] #number of months after a rabbit die'''
+        new=0
+        for x in range(1,len(age_pop)):
+            new+=age_pop[x]
+        dead=age_pop.pop()
+        age_pop.appendleft(new)
+
+    return sum(age_pop)
 
 m=3
 n=6
-for x in range(1,n+1):
+age_pop=deque([])
+for x in range(m):
+    age_pop.append(0)
+for x in range(0,n+1):
     N=fibonacci_variant(x)
     print x, N
