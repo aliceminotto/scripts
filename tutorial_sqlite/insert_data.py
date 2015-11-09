a04c5647ec2bbdd86e749b1efe072329514830d7
aliceminotto <i.m.e@hotmail.it>
2015-11-09 17:07:01 +0000

    same

--- tutorial_sqlite/insert_data.py
+++ tutorial_sqlite/insert_data.py
@@ -5,17 +5,22 @@ import sqlite3
 conn = sqlite3.connect('provadb.db')
 print "Opened database successfully";
 
-conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
-      VALUES (1, 'Paul', 32, 'California', 20000.00 )");
+prova=9
 
 conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
-      VALUES (2, 'Allen', 25, 'Texas', 15000.00 )");
+      VALUES (5, 'Paul', 32, 'California', 20000.00 )");
 
 conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
-      VALUES (3, 'Teddy', 23, 'Norway', 20000.00 )");
+      VALUES (6, 'Allen', 25, 'Texas', 15000.00 )");
 
 conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
-      VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 )");
+      VALUES (7, 'Teddy', 23, 'Norway', 20000.00 )");
+
+conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (8, 'Mark', 25, 'Rich-Mond ', 65000.00 )");
+
+conn.execute("insert into company (id, name, age, address, salary) \
+	values (prova,'anna',42,'texas',10.0)");
 
 conn.commit()
 print "Records created successfully";
