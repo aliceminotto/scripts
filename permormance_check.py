17e987ab457c81055aa2788ea0db4a6af864a721
aliceminotto <i.m.e@hotmail.it>
2015-11-26 11:30:54 +0000

    fixed code and my method it is better when digits are just in the middle

--- permormance_check.py
+++ permormance_check.py
@@ -5,15 +5,16 @@ ex_string='pts37plotdata.p'
 
 def use_strip(stringa):
     alf='qwertyuiopasdfghjklzxcvbnm.'
-    jump_n=int(ex_string.strip(alf))
-    print jump_n
+    jump_n=int(stringa.strip(alf))
+    #print jump_n
 def use_translate(stringa):
     import string
     table=string.maketrans('','')
     del_numb=table.translate(table,string.digits)
     jump_n=stringa.translate(table,del_numb)
-    print jump_n
+    #print jump_n
 
- print timeit.timeit(use_strip(ex_string))
-
-print timeit.timeit(use_translate(ex_string))
+print 'strip method'
+print timeit.timeit("use_strip(ex_string)",setup="from __main__ import use_strip, ex_string")
+print 'translate method'
+print timeit.timeit("use_translate(ex_string)",setup="from __main__ import use_translate, ex_string")
