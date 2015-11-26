fc43264b6caafefdc3b49abb049899f22da3d6b3
aliceminotto <i.m.e@hotmail.it>
2015-11-26 11:15:03 +0000

    add launch files and performance check for time

---
+++ launch_update_c_db.sh
@@ -0,0 +1,2 @@
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/tsl/software/testing/gsl/1.16/x86_64/bin/lib
+bsub -q TSL-Prod128 -We 1 "source python-2.7.4; python /usr/users/TSL_20/minottoa/scripts/update_c_db.py /usr/users/TSL_20/minottoa/change_DT_Qi/DT5000/"
