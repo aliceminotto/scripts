a77cde4865d0073a42385710a2be6cb1e1da57e5
aliceminotto <i.m.e@hotmail.it>
2015-12-02 15:42:34 +0000

    c++ hello world

---
+++ tutorial_c/hello_world.cpp
@@ -0,0 +1,10 @@
+// my first c++ program
+#include <iostream> //enable the use of standard input an standard output
+using namespace std;
+
+int main()
+//declaration of a function, main is called when the program runs
+{
+cout << "Hello World!\n";
+cout << "I'ma C++ program";
+}
