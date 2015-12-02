742f694c7fefa03baa8e08afabe8f7f5345684b3
aliceminotto <i.m.e@hotmail.it>
2015-12-02 17:31:29 +0000

    basic code w/ input and output

---
+++ tutorial_c/input_output.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int eta;
+    cout << "Insert your age\n";
+    cin >> eta;
+    cout << "the double of " << eta << " is " << eta*2 << "\n";
+}
